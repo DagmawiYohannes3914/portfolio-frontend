fb4a9a5d1ea006b9fa64fd8169601e92d4f136fe
Dagmawi Yohannes <dagmawiyohannes323@gmail.com>
2024-06-25 14:32:29 +0300

    Adding css and app js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import AboutPage from "./pages/AboutPage";
+import ProjectsPage from "./pages/ProjectsPage";
+import ResumePage from "./pages/ResumePage";
+import ContactPage from "./pages/ContactPage";
+import BlogPage from "./pages/BlogPage";
+import BlogDetailPage from "./pages/BlogDetailPage";
+import Navbar from "./components/Navbar";
+import "./styles/index.css";
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className={darkMode ? "dark" : ""}>
+        <div className="App bg-white dark:bg-gray-900 text-black dark:text-white min-h-screen">
+          <Navbar toggleDarkMode={toggleDarkMode} />
+          <Routes>
+            <Route path="/" exact element={<HomePage />} />
+            <Route path="/about" element={<AboutPage />} />
+            <Route path="/projects" element={<ProjectsPage />} />
+            <Route path="/resume" element={<ResumePage />} />
+            <Route path="/contact" element={<ContactPage />} />
+            <Route path="/blog" exact element={<BlogPage />} />
+            <Route path="/blog/:id" element={<BlogDetailPage />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
   );
 }
 
